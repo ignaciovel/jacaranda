a094b9ff39c3498c281b47ce768cd7a46638601f
heyvn <ignaciovelazco12@gmail.com>
2019-02-07 01:02:31 -0300

    hot fix finals

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,10 +2,10 @@
     <md-toolbar id="main-menu" md-elevation="1">
         <md-layout md-row>
             <md-layout md-sm-column>
-                <md-layout class="md-medium-hide" md-align="left" style="flex-wrap:nowrap;" md-sm-column>
                 <router-link :to="{name: 'home'}" style="width:inherit;">
-                    <img id="index-logo" class="menu-logo" src="images/logo.jpg" style=""/>
+                    <img id="index-logo" class="menu-logo" src="images/logo.jpg" />
                 </router-link>
+                <md-layout class="md-medium-hide" md-align="left" style="flex-wrap:nowrap;" md-sm-column>
                 <router-link v-for="(value,key) in this.$root.dataPage.NavPage" v-bind:key="key" :to="{name: key}" class=" md-button top-menu-button  no-background-on-hover md-hide-xsmall
                                     md-hide-small md-button-nav" active-class="current-page"> {{ value }}</router-link>
                 <md-button class="top-menu-button  no-background-on-hover md-hide-xsmall
